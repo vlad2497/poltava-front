35db15ce768371cefa7b40ca61868ac42e53754c
Vladislav <vladislavmatkovskiy2015@mail.ru>
2021-04-10 17:31:03 +0300

    was installed main modules

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  publicPath: "./",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src/components"),
+        "@api": path.resolve(__dirname, "src/api"),
+        "@constants": path.resolve(__dirname, "src/constants"),
+        "@plugins": path.resolve(__dirname, "src/plugins"),
+        "@helpers": path.resolve(__dirname, "src/helpers"),
+      },
+      extensions: [".js", ".vue", ".json"],
+    },
+  },
+};
